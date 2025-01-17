fa8863628019b69cd5525f62321c9f3426206865
JoKyuHoon <rbgnsl56@naver.com>
2025-01-17 18:30:12 +0900

    0117

--- chap15_collectionFrameWork/src/_06_tree/T01_treeSet복습.java
+++ chap15_collectionFrameWork/src/_06_tree/T01_treeSet복습.java
@@ -1,5 +1,6 @@
 package _06_tree;
 
+import java.util.Scanner;
 import java.util.Set;
 import java.util.TreeSet;
 
@@ -7,10 +8,19 @@ public class T01_treeSet복습 {
 
 	public static void main(String[] args) {
 		Set<Integer> set = new TreeSet();
+		Scanner sc = new Scanner(System.in);
 		
-
-
+		System.out.println("금액 : ");
+		
+		int money = sc.nextInt() / 1000;
+		for(int i=0; i<money; i++) {
+			set.clear();
+		while(set.size() < 6 ) {
+			set.add((int)(Math.random()*45) + 1 );
+		}
+		System.out.println(set);
 
 	}
 
+}
 }
---
+++ chap15_collectionFrameWork/src/_06_tree/_for.java
@@ -0,0 +1,18 @@
+package _06_tree;
+
+public class _for {
+
+	public static void main(String[] args) {
+	
+		int result = 0;
+		
+		for(int i=1; i <= 0; i++) {
+			
+			result += i;
+			
+			System.out.println(result);
+		}
+
+	}
+
+}
